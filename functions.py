4eb7547d77f3b5b932803923a33b7b5f7e2b48c6
juhamikael <contact.juhamusic@gmail.com>
2021-08-12 00:56:32 +0300

    #2 Format code

---
+++ functions.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+
+def rename_remove_date(list, inp, project_name):
+    files_to_rename = []
+    for files in list:
+        if files.startswith(project_name):
+            files_to_rename.append(files)
+    for files in files_to_rename:
+        date_name_len = 21
+        total_worthless_len = len(inp) + date_name_len
+        os.rename(files, files[total_worthless_len:])
+
+
+def move_files(list, project_name, new_path):
+    project_stems_to_move = []
+    for file in list:
+        if file.startswith(project_name):
+            project_stems_to_move.append(file)
+    for file in project_stems_to_move:
+        shutil.move(file, new_path)
--- main.py
+++ main.py
@@ -1,61 +1,33 @@
 import os
 import shutil
 from subprocess import run
+from functions import rename_remove_date, move_files
+
 import configparser
 
-#####
+# Configuration
 config = configparser.ConfigParser()
-
 read_config = configparser.ConfigParser()
 read_config.read("./config.ini")
-path = read_config.get("Path", "flstudio_data_path")
-fl_date_name_len = 21
-#####
+path = read_config.get("Path", "FL_Studio_data_path")
 
-
+# Variables
 project_name = input(f"Type your project name: ")
 search_string = project_name.replace(" ", "_")
 search_string_to_lower = search_string.lower()
-
-# project_name = "pythontest"
-
-listdir = os.listdir(path)
-stems_renamed = [];
-for rename_files in listdir:
-    rename_files.lower()
-    rename_files.replace(" ", "_")
-
-project_stems = []
-for file in listdir:
-    if file.startswith(project_name):
-        project_stems.append(file)
-
-#######################
-#Make new folder with project name
 new_folder = f"cd {path} &&" \
              f"mkdir {search_string_to_lower}"
+new_path = f"{path}/{search_string_to_lower}"
+
+# Changing working path so new folder can be made
 os.chdir(path)
 run(new_folder, shell=True)
 
-new_path = f"{path}/{search_string_to_lower}"
-# cd_new_path = f"cd {path}/{project_name}"
+# Move file
+first_ls = os.listdir(path)
+move_files(first_ls, project_name, new_path)
 
-#######################
-#Move files to new path
-for files in project_stems:
-    shutil.move(files, new_path)
-
+# Rename files (remove date from the name)
 os.chdir(new_path)
-
-new_listdir = os.listdir(new_path)
-project_stems = []
-for file in new_listdir:
-    if file.startswith(project_name):
-        project_stems.append(file)
-
-for files in project_stems:
-    project_name_len = len(search_string_to_lower)
-    date_name_len = 21
-    total_worthless_len = project_name_len + date_name_len
-    os.rename(files, files[total_worthless_len:])
-
+second_ls = os.listdir(new_path)
+rename_remove_date(second_ls, search_string_to_lower, project_name)
