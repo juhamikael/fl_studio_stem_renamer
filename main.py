8690001c394b566fd4246902bcd445c279310d35
Juhamikael <contact.juhamusic@gmail.com>
2021-11-14 17:31:54 +0200

    Add everyhing back

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from subprocess import run
+from functions import rename_remove_date, move_files
+import configparser
+
+# Configuration
+read_config = configparser.ConfigParser()
+read_config.read("./config.ini")
+path = read_config.get("Path", "FL_Studio_data_path")
+print(path)
+
+# Variables
+project_name = input(f"Type your project name: ")
+search_string = project_name.replace(" ", "_")
+search_string_to_lower = search_string.lower()
+new_folder = f"cd {path} &&" \
+             f"mkdir {search_string_to_lower}"
+new_path = f"{path}/{search_string_to_lower}"
+# Ignore lowercase
+
+# Changing working path so new folder can be made
+os.chdir(path)
+run(new_folder, shell=True)
+
+first_ls = os.listdir(path)
+
+# Lowercase files
+
+
+# Move files
+move_files(first_ls, project_name, new_path)
+
+# Rename files (remove date from the name)
+os.chdir(new_path)
+second_ls = os.listdir(new_path)
+rename_remove_date(second_ls, search_string_to_lower, project_name)
