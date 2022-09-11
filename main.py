712ae036d6be053b92a843974e907e51dba34b1d
Juhamikael <contact.juhamusic@gmail.com>
2022-09-11 14:37:57 +0300

    Turn into object

--- main.py
+++ main.py
@@ -1,35 +1,64 @@
 import os
 from subprocess import run
-from functions import rename_remove_date, move_files
 import configparser
+import shutil
 
-# Configuration
-read_config = configparser.ConfigParser()
-read_config.read("./config.ini")
-defaulPath = read_config.get("Path", "default_path")
-customPath = read_config.get("Path", "custom_path")
-path = f"{customPath}{defaulPath}"
-print("Current path =", path)
 
-# Variables
-project_name = input(f"Type your project name: ")
-search_string = project_name.replace(" ", "_")
-search_string_to_lower = search_string.lower()
-new_folder = f"cd {path} &&" \
-             f"mkdir {search_string_to_lower}"
-new_path = f"{path}/{search_string_to_lower}"
-# Ignore lowercase
+class StemRenamer:
+    def __init__(self):
+        self.path = self.read_config_()
+        self.project_name = input(f"Type your project name: ")
+        self.search_string = self.project_name.replace(" ", "_")
+        self.search_string_to_lower = self.search_string.lower()
+        self.new_folder = f"cd {self.read_config_()} && " \
+                          f"mkdir {self.search_string_to_lower}"
+        self.new_path = fr"{self.path}\{self.search_string_to_lower}"
 
-# Changing working path so new folder can be made
-os.chdir(path)
-run(new_folder, shell=True)
+    @staticmethod
+    def read_config_():
+        read_config = configparser.ConfigParser()
+        read_config.read("./config.ini")
+        defaulPath = read_config.get("Path", "default_path")
+        customPath = read_config.get("Path", "custom_path")
+        path = f"{customPath}{defaulPath}"
+        return path
 
-first_ls = os.listdir(path)
+    def rename_files(self, file_list: list, inp):
+        os.chdir(self.new_path)
+        files_to_rename = []
+        for file in file_list:
+            # if file string contains search string
+            if file.startswith(self.project_name):
+                files_to_rename.append(file)
 
-# Move files
-move_files(first_ls, project_name, new_path)
+        for file in files_to_rename:
+            date_name_len = 21
+            total_worthless_len = len(inp) + date_name_len
+            os.rename(file, file[total_worthless_len:])
 
-# Rename files (remove date from the name)
-os.chdir(new_path)
-second_ls = os.listdir(new_path)
-rename_remove_date(second_ls, search_string_to_lower, project_name)
+    def run_create_folder(self):
+        print(self.new_folder)
+        os.chdir(self.path)
+        run(self.new_folder, shell=True)
+
+    def run_move_files(self):
+        project_stems_to_move = []
+        file_list = os.listdir(self.path)
+        for file in file_list:
+            if file.startswith(self.project_name):
+                project_stems_to_move.append(file)
+        for file in project_stems_to_move:
+            shutil.move(file, self.new_path)
+
+    def run_rename_files(self):
+        file_list = os.listdir(self.new_path)
+        self.rename_files(file_list, self.search_string_to_lower)
+
+    def run(self):
+        self.run_create_folder()
+        self.run_move_files()
+        self.run_rename_files()
+
+
+sr = StemRenamer()
+sr.run()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('VERSION', 'r') as version_file:
+    version = version_file.readline().strip()
+
+setup(
+    name="stem_renamer",
+    version=version,
+    author="Juha Savikko",
+    package=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "kivy"
+    ]
+)
